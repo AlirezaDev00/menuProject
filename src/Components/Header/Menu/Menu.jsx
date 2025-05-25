c970ec2a6c7b54d25babcda54c9371164cb473d4
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-05-25 16:05:49 +0330

    update styles

--- src/Components/Header/Menu/Menu.jsx
+++ src/Components/Header/Menu/Menu.jsx
@@ -3,29 +3,29 @@ function Menu() {
     <nav className="flex items-center justify-center">
       <ul className="flex gap-4">
         <li>
-          <button className="px-4 py-2 transition-colors duration-250 ease-linear rounded-md group hover:bg-main cursor-pointer">
-            <span className="text-xl text-main transition-colors duration-250 ease-linear group-hover:text-default">
+          <button className="btn group">
+            <span className="btn_content">
               All
             </span>
           </button>
         </li>
         <li>
-          <button className="px-4 py-2 transition-colors  duration-250 ease-linear rounded-md group hover:bg-main cursor-pointer">
-            <span className="text-xl text-main transition-colors duration-250 ease-linear  group-hover:text-default">
+          <button className="btn group">
+            <span className="btn_content">
               dsdsddssd
             </span>
           </button>
         </li>
         <li>
-          <button className="px-4 py-2 transition-colors  duration-250 ease-linear rounded-md group hover:bg-main cursor-pointer">
-            <span className="text-xl text-main transition-colors duration-250 ease-linear  group-hover:text-default">
+          <button className="btn group">
+            <span className="btn_content">
               All
             </span>
           </button>
         </li>
         <li>
-          <button className="px-4 py-2 transition-colors  duration-250 ease-linear rounded-md group hover:bg-main cursor-pointer">
-            <span className="text-xl text-main transition-colors duration-250 ease-linear  group-hover:text-default">
+          <button className="btn group">
+            <span className="btn_content">
               All
             </span>
           </button>
